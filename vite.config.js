8afbcab2ed8160b403c510c6c5ff53d549d561a6
Valentin Huth <valentin.huth@maibornwolff.de>
2024-11-17 19:10:11 +0100

    Added base tag in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     root: '.',          // Setzt den Projektstamm (wo index.html liegt)
+    base: '/portfolio-website',
     build: {
         outDir: 'dist', // Gibt an, wohin die Produktionsdateien exportiert werden
     },
