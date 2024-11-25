d89d16bd7e80131daa9460e8aaa82d0a181ce300
Valentin Huth <valentin.huth@maibornwolff.de>
2024-11-25 15:22:48 +0100

    Edited commands

--- src/main.js
+++ src/main.js
@@ -19,26 +19,56 @@ document.addEventListener('DOMContentLoaded', () => {
         // Befehlsverarbeitung
         switch (command.toLowerCase()) {
             case 'help':
-                addOutput('List of all available commands: \nabout\nhelp\nprojects\nwhois\n ');
+                addOutput('help');
                 break;
             case 'about':
                 addOutput('This website is about my software engineering projects.\n ');
                 break;
             case 'whois':
-                addOutput('Hey I´m Valentin!\nI am an 21 y.o. dual Student at Nordakademie Elmshorn in cooperation with MaibornWolff Hamburg. \nOn this Website you get a look of my experience in software engineering!\n ');
+                addOutput('Hey I´m Valentin!\nI am an 21 y.o. dual Student at Nordakademie Elmshorn in cooperation with MaibornWolff Hamburg. On this Website you get a look of my experience in software engineering!\n ');
                 break;
             case 'projects':
-                addOutput('This is a list of my projects.\n ');
+                addOutput('projects');
                 break;
             default:
                 addOutput(`Unbekannter Befehl: ${command}\n `);
         }
     }
 
+    const commands = [
+        '~ about        Description about this website.',
+        '~ help         List all available commands.',
+        '~ whois        Description about myself.',
+        '~ projects     List of my projects. \n ',
+    ];
+
+    const projects = [
+        '- Calculator           An calculator application build with Java.',
+        '- WeatherApp           An weather web application build with HTML, JavaScript and CSS.',
+        '- Game-Collection      One Application which contains Pong, TicTacToe and Snake, build with Java. \n '
+    ];
+
     function addOutput(text) {
+
+        if (text === 'help') {
+            commands.forEach(command => {
+                const line = document.createElement('div');
+                line.textContent = command;
+                output.appendChild(line);
+                output.scrollTop = output.scrollHeight;
+            });
+        } else if (text === 'projects') {
+            projects.forEach(project => {
+                const line = document.createElement('div');
+                line.textContent = project;
+                output.appendChild(line);
+                output.scrollTop = output.scrollHeight;
+            });
+        } else {
         const line = document.createElement('div');
         line.textContent = text;
         output.appendChild(line);
         output.scrollTop = output.scrollHeight;
+        };
     }
 });
